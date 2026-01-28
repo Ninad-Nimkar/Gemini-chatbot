8fdced832c336e0d47ffee97ffbb1bef93ccfa7a
Gudboy <ninadnimkar2021@gmail.com>
2026-01-28 10:59:40 +0530

    removed the key

--- chatboy.py
+++ chatboy.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client = genai.Client(api_key="AIzaSyDvtUKR01gwB7SZkiG8p2aC29E6IEiCORg")
+client = genai.Client(api_key="")
 prompt = input("prompt: ")
 
 response = client.models.generate_content(
